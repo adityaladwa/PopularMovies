6a8d0d791e2d71ef41e1a130afbabd29e9da3ea3
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-02-03 14:36:05 +0530

    Added Okhttp as dependency

--- app/src/main/java/com/ladwa/aditya/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/ladwa/aditya/popularmovies/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 
@@ -21,7 +22,7 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         ButterKnife.bind(this, view);
-
+        Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT).show();
 
         return view;
     }
