396fd90d76f27c80e70541735d8b0e6bffa049ae
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-02-03 15:21:34 +0530

    Binded views to adapters

--- app/src/main/java/com/ladwa/aditya/popularmovies/adapter/RecyclerViewMoviesAdapter.java
+++ app/src/main/java/com/ladwa/aditya/popularmovies/adapter/RecyclerViewMoviesAdapter.java
@@ -1,8 +1,15 @@
 package com.ladwa.aditya.popularmovies.adapter;
 
+import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.ladwa.aditya.popularmovies.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Aditya on 03-Feb-16.
@@ -25,9 +32,13 @@ public class RecyclerViewMoviesAdapter extends RecyclerView.Adapter<RecyclerView
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.imageview_poster)
+        private ImageView imageView;
+
 
         public MyViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this,itemView);
         }
     }
 }
