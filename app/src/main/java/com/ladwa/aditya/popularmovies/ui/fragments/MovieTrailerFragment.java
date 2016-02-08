c909169c76bdea7cb85c53b8a6b8d43824cacfeb
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-02-08 22:06:30 +0530

    Added layout for movie trailer and reviews

--- app/src/main/java/com/ladwa/aditya/popularmovies/ui/fragments/MovieTrailerFragment.java
+++ app/src/main/java/com/ladwa/aditya/popularmovies/ui/fragments/MovieTrailerFragment.java
@@ -27,7 +27,7 @@ public class MovieTrailerFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_plot, container, false);
+        View view = inflater.inflate(R.layout.fragment_movie_trailer, container, false);
         ButterKnife.bind(this, view);
 
         return view;
