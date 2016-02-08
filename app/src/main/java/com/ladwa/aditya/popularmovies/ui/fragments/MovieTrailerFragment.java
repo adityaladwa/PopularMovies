e9839e108bad00b4da6d2300c5b753cff7e4064e
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-02-09 00:03:05 +0530

    Successfully fetching the video result

--- app/src/main/java/com/ladwa/aditya/popularmovies/ui/fragments/MovieTrailerFragment.java
+++ app/src/main/java/com/ladwa/aditya/popularmovies/ui/fragments/MovieTrailerFragment.java
@@ -3,15 +3,18 @@ package com.ladwa.aditya.popularmovies.ui.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ladwa.aditya.popularmovies.R;
 import com.ladwa.aditya.popularmovies.data.api.MovieApi;
 import com.ladwa.aditya.popularmovies.data.api.ServiceGenerator;
 import com.ladwa.aditya.popularmovies.data.model.MovieVideoListModel;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import rx.Observer;
 import rx.Subscription;
@@ -23,8 +26,11 @@ import rx.schedulers.Schedulers;
  */
 public class MovieTrailerFragment extends Fragment {
 
+    private static final String LOG_TAG = MovieTrailerFragment.class.getSimpleName();
     private MovieApi movieApi;
     private Subscription videoSubscription;
+    @Bind(R.id.tvTrailer)
+    TextView textViewTrailer;
 
 
     public MovieTrailerFragment() {
@@ -36,7 +42,6 @@ public class MovieTrailerFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_movie_trailer, container, false);
         ButterKnife.bind(this, view);
 
-
         movieApi = ServiceGenerator.createService(MovieApi.class);
         videoSubscription = movieApi.getMovieTrailerRx("281957", getString(R.string.api_key))
                 .subscribeOn(Schedulers.io())
@@ -45,7 +50,7 @@ public class MovieTrailerFragment extends Fragment {
                 .subscribe(new Observer<MovieVideoListModel>() {
                     @Override
                     public void onCompleted() {
-
+                        Log.d(LOG_TAG, "Completed loading movie videos");
                     }
 
                     @Override
@@ -55,7 +60,9 @@ public class MovieTrailerFragment extends Fragment {
 
                     @Override
                     public void onNext(MovieVideoListModel movieVideoListModel) {
-
+                        for (int i = 0; i < movieVideoListModel.getResults().size(); i++) {
+                            textViewTrailer.append(movieVideoListModel.getResults().get(i).getVideoID() + "\n");
+                        }
                     }
                 });
 
