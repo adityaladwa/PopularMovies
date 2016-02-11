8f1a984de9509d4fb1028443c4834179a0ee75c0
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-02-11 22:04:40 +0530

    Created RecycleView Layout for Movie Review

--- app/src/main/java/com/ladwa/aditya/popularmovies/data/api/MovieApi.java
+++ app/src/main/java/com/ladwa/aditya/popularmovies/data/api/MovieApi.java
@@ -1,8 +1,9 @@
 package com.ladwa.aditya.popularmovies.data.api;
 
 
-import com.ladwa.aditya.popularmovies.data.model.MovieVideoListModel;
 import com.ladwa.aditya.popularmovies.data.model.MovieResultListModel;
+import com.ladwa.aditya.popularmovies.data.model.MovieReviewListModel;
+import com.ladwa.aditya.popularmovies.data.model.MovieVideoListModel;
 
 import retrofit.http.GET;
 import retrofit.http.Path;
@@ -20,6 +21,9 @@ public interface MovieApi {
 
     @GET("movie/{id}/videos")
     Observable<MovieVideoListModel> getMovieTrailerRx(@Path("id") String id, @Query("api_key") String apikey);
+
+    @GET("movie/{id}/reviews")
+    Observable<MovieReviewListModel> getMovieReviewRx(@Path("id") String id, @Query("api_key") String apikey);
 
 }
 
