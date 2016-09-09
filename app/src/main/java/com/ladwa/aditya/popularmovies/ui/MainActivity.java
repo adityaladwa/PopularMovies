91258c7eae75c17e2208e941c3de5901eee21fbd
Aditya <ladwa.aditya@gmail.com>
2016-09-09 13:48:41 +0530

    Fect: Getting Ads Properly

--- app/src/main/java/com/ladwa/aditya/popularmovies/ui/MainActivity.java
+++ app/src/main/java/com/ladwa/aditya/popularmovies/ui/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.ladwa.aditya.popularmovies.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        MobileAds.initialize(getApplicationContext(),"ca-app-pub-5118635494203698~4052638391");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
     }
 
 }
